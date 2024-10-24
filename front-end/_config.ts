c2dc874e090298a4d20e2cfdab967bb6da70e6ca
Samir <samirxnova@gmail.com>
2024-10-24 14:38:05 +0530

    config

---
+++ front-end/_config.ts
@@ -0,0 +1,15 @@
+const config = {
+  metadata: {
+    title: 'Strk AI Builder',
+    // eslint-disable-next-line quotes
+    description: "Strk AI Builder, to build through intelligence",
+    keywords:
+      'react.js, react.js-template, react.js-boilerplate, typescript, shadcn/ui, tailwindcss, vite, swc, eslint, prettier, husky, commitlint, lint-staged'
+  },
+  server: {
+    host: 'localhost',
+    port: 3000
+  }
+};
+
+export default config;
